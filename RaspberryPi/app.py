6430acf0dc364ccd30f6d3919437908587a41bf3
adr1anb97 <93594725+adr1anb97@users.noreply.github.com>
2023-12-21 17:11:48 +0100

    Update app.py

--- RaspberryPi/app.py
+++ RaspberryPi/app.py
@@ -1,1 +1,12 @@
+from flask import Flask, jsonify
+from your_sensor_script import get_data_from_sensors
 
+app = Flask(__name__)
+
+@app.route('/api/sensor-data', methods=['GET'])
+def sensor_data():
+    data = get_data_from_sensors()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
